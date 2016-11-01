1afcad1200d9876fb40233d110fc42a59f72457d
Grace Lee <eunheh@gmail.com>
2016-10-31 22:02:06 -0400

    limited pics to 5

--- src/js/menuicon.js
+++ src/js/menuicon.js
@@ -1,32 +1,45 @@
 import $ from 'jquery';
 
+import { getMenuData } from "./api";
+
 var iconTemplate = `
   <ul class="iconList">
-    <li><img class="icon icon1 disclosure1" id="allergyIcon" src="./images/exclamation.png"></li>
-    <li><img class="icon icon2 disclosure2" id="veganIcon" src="./images/broccoli.png"></li>
-    <li><img class="icon icon3 disclosure3" id="spicyIcon" src="./images/chili.png"></li>
-    <li><img class="icon icon4 disclosure4" id="favIcon" src="./images/star.png"</li>
+
+    <li class="icon1"><img class="icon" id="allergyIcon" src="./images/exclamation.png"><div class="disclosure1">This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance.</div></li>
+
+    <li class="icon2"><img class="icon icon2" id="veganIcon" src="./images/broccoli.png"><div class="disclosure2">This item contains no meat and has been prepared without the use of animal products.</div></li>
+
+    <li class="icon3"><img class="icon icon3" id="spicyIcon" src="./images/chili.png"><div class="disclosure3">This item is spicy, please handle with care and drink lots of water.</div></li>
+
+    <li class="icon4"><img class="icon icon4" id="favIcon" src="./images/star.png"><div class="disclosure4">We have been doing this a long time and this item has become one of our favorites.</div></li>
+
   </ul>`;
 
-function menuIcon (event) {
-  var target = $(event.target);
-  console.log(target[0].id);
+function getIconInfo (icons) {
+  var appIcon = icons.appetizers;
+  var entreeIcon = icons.entrees;
+  var sideIcon = icons.sides;
 
-  var disclosure1 = `<div class="disclosure1">This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance.</div>`
-  var disclosure2 = `<div class="disclosure2">This item contains no meat and has been prepared without the use of animal products.</div>`
-  var disclosure3 = `<div class="disclosure3">This item is spicy, please handle with care and drink lots of water.</div>`
-  var disclosure4 = `<div class="disclosure4">We have been doing this a long time and this item has become one of our favorites.</div>`
+  console.log(appIcon);
+  console.log(entreeIcon);
+  console.log(sideIcon);
 
-    if (target[0].id == "allergyIcon") {
-      $(".icon1").addClass("disclosure1");
-  } else if (target[0].id == "veganIcon") {
-      $(".icon2").addClass("disclosure2");
-  } else if (target[0].id == "spicyIcon") {
-      $(".icon3").addClass("disclosure3");
-  } else if (target[0].id == "favIcon") {
-      $(".icon4").addClass("disclosure4");
+  if (appIcon.allergies === 1) {
+    return `${icon1}`
+  }
+  if (appIcon.vegan === 1) {
+    return `${icon2}`
+  }
+  if (appIcon.spicy === 1) {
+    return `${icon3}`
+  }
+  if (appIcon.favorites === 1) {
+    return `${icon4}`
   }
 
-}
+};
 
-export { iconTemplate, menuIcon};
+
+getMenuData().then(getIconInfo);
+
+export { iconTemplate, getIconInfo };
